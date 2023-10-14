89a45cbe37e427e598699188cf91a3019b9311a9
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2023-10-14 14:53:50 -0400

    red flag do red flag things

---
+++ misc/red_flag.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _input(event):
+	if get_overlapping_bodies().size() > 0:
+		get_tree().change_scene_to_file("res://levels/level_02.tscn")
