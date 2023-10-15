e7ef512195854d8a3432275dfe116f302f3fe98a
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2023-10-15 02:35:59 -0400

    commit before i accidentally delete another file

---
+++ cutscenes/TextEdit.gd
@@ -0,0 +1,14 @@
+extends Label
+
+var timer : float = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	timer += delta
+	if timer > 5:
+		self.text = "AAA"
