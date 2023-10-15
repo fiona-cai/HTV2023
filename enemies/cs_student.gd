3ffe9203b8f667b1584108c86b80b7bdf35645c7
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2023-10-14 21:38:02 -0400

    GODDAMIT CS KIDS STOP SPINNING

--- enemies/cs_student.gd
+++ enemies/cs_student.gd
@@ -1,11 +1,27 @@
 extends RigidBody2D
 
+const MAX_VEL_LOL = 50
+const SPEED = 5000
+var steps = 0
+var dir = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _integrate_forces(state):
+	# Set the angular velocity to zero to prevent rotation
+	angular_velocity = 0
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+
+	var velocity = Vector2(delta*dir*SPEED, 0)
+	steps += delta
+	if abs(velocity.x) > MAX_VEL_LOL:
+		dir = dir*-1
+		steps = 0
+	var force = Vector2(100, 0)  # Replace with the desired force vector
+	print(velocity)
+	linear_velocity = velocity
